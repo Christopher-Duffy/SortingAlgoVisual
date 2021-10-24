9f1b9f7a1eb5b065fd9f99b994fa96f4ca9bdd9c
Chris Duffy <christopher.b.duffy@gmail.com>
2021-10-24 12:58:48 -0700

    Added Basic Visualization for Random Numbers

--- resources/scripts/logic.js
+++ resources/scripts/logic.js
@@ -3,8 +3,23 @@ var valuesToSort=[];
 function doSort(){
     alert("I am now doing the sort");
 }
-function resetSort(){
+function resetValues(){
     valuesToSort=makeRandomArray(100);
+}
+
+function renderValues(){
+    var visualizer = document.getElementById("visualizer");
+    var width= visualizer.clientWidth / valuesToSort.length;
+    var height = visualizer.clientHeight;
+    deleteElementDivs();
+    makeDivsFromArray(valuesToSort, width, height);
+}
+
+function deleteElementDivs(){
+   var divs= document.getElementsByClassName("element");
+   while(divs[0]){
+       divs[0].parentNode.removeChild(divs[0]);
+   }
 }
 
 function makeRandomArray(size){
@@ -13,4 +28,18 @@ function makeRandomArray(size){
         arr.push(Math.floor(Math.random()*100));
     }
     return arr;
+}
+
+function makeDivsFromArray(arr,width,height){
+    arr.forEach(element => {
+        document.getElementById("visualizer").append(makeElement(width,height*element/100));
+    });
+}
+
+function makeElement(width,height){
+    var div = document.createElement("div");
+    div.style.width=width+"px";
+    div.style.height=height+"px";
+    div.classList.add("element");
+    return div;
 }
